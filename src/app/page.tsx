4aaff6d3f3f1739c7e583eca1d7e6548bd221adf
Aleyna Eser <aleynaeser345.ma@gmail.com>
2026-03-18 02:32:28 +0300

    [Update]: adjust text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,19 +19,19 @@ export default async function Home() {
               className='border-stroke hover:bg-accent flex min-h-44 flex-col justify-between border-[0.5px] px-6 py-5'
             >
               <div>
-                <p className='text-neutral mb-2 text-xs tracking-[0.16em]'>{item.author}</p>
+                <p className='text-secondary mb-2 text-xs tracking-[0.16em]'>{item.author}</p>
 
                 <p className='text-primary mb-2 text-lg/tight font-bold tracking-[0.18em] uppercase italic'>
                   <strong>&quot;{item.title}&quot;</strong>
                 </p>
 
-                <p className='text-secondary line-clamp-2 text-xs/relaxed'>{item.description}</p>
+                <p className='text-neutral line-clamp-2 text-xs/relaxed'>{item.description}</p>
               </div>
 
               <footer className='mt-8 flex items-center justify-between'>
                 <div className='flex items-center gap-2 text-xs uppercase'>
                   {item.categories.map((c) => (
-                    <div key={c.id} className='bg-accent text-secondary rounded-full px-3 py-1'>
+                    <div key={c.id} className='bg-accent text-neutral rounded-full px-3 py-1'>
                       {c.name}
                     </div>
                   ))}
